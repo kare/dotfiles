749b48d23a8f44a0fad6f69852a77f94da2b1f2e
Kare Nuorteva <kare.nuorteva@me.com>
2014-11-05 22:06:32 +0200

    Save preferences per application
    Signed-off-by: Kare Nuorteva <kare.nuorteva@me.com>

--- bin/osx.sh
+++ bin/osx.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
+export PYTHONPATH="/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python"
+
+function CFPreferencesAppSynchronize() {
+    python - <<END
+from Foundation import CFPreferencesAppSynchronize
+CFPreferencesAppSynchronize('$1')
+END
+}
+
+###########################################################
+# Safari
+###########################################################
+
 # Show Safari status bar
 defaults write com.apple.Safari ShowStatusBar -boolean true
+
+CFPreferencesAppSynchronize "com.apple.Safari"
